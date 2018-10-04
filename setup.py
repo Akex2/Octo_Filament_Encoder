117f3c4b7306feaad7001a757edc6cfb243965c1
Akex2 <schointss@gmail.com>
2018-10-04 23:08:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/Akex2/Octo_Filament_Encoder"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["Rpi_encoder"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
